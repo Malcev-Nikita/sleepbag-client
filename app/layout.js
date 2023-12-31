34e3db84e1a8e59d9523ba78763fa5d95b63990d
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-12-31 16:37:51 +0400

    fix main page Front

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body className='h-[100vh]'>
+      <body>
         <Header/>
 
         {children}
