7e151045bf92922e41b9faf0e8ad9b702149f90f
Malcev Nikita <nik2004itamalcev@gmail.com>
2024-02-20 15:42:18 +0400

    Вывел страницу каталога из cms

--- app/catalog/page.js
+++ app/catalog/page.js
@@ -1,8 +1,6 @@
 import Image from 'next/image'
-import { getFAQ } from '@/services/IndexPage'
-import Questions from '@/components/footer/questions'
-import FAQ from '@/components/footer/faq'
 import Catalog from '@/components/catalog/catalog'
+import { getCatalogPageContent } from '@/services/catalog/page'
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,15 +8,20 @@ export const metadata = {
 }
 
 export default async function Page() {
-  const faq = await getFAQ();
+  const data = await getCatalogPageContent()
+
+  const header = () => ({__html: data.data.attributes.header})
+  const description = () => ({__html: data.data.attributes.description})
+  const categories_header = () => ({__html: data.data.attributes.categories_header})
+  const categories_button = () => ({__html: data.data.attributes.categories_button})
 
   return (
     <main className="">        
         <header className='catalog_header'>
           <Image className='catalog_header_img aspect-video w-[100%] h-[100vh] object-cover' src='/bg/catalog_bg_img.png' width={1920} height={1080}/>
           <div className='container z-5 absolute translate-[-50%] translate-x-[-50%] left-[50%]'>
-            <h1>КАТАЛОГ</h1>
-            <p>Всё для вашего автомобиля - широкий выбор, надёжность и качество!</p>
+            <h1 dangerouslySetInnerHTML={header()} />
+            <p dangerouslySetInnerHTML={description()} />
           </div>
         </header>
 
@@ -26,7 +29,7 @@ export default async function Page() {
           
           <div className='section_header container m-auto'>
             <div className='header_line'></div>
-            <h1>ВЫБОР КАТЕГОРИИ</h1>
+            <h1 dangerouslySetInnerHTML={categories_header()} />
           </div>
           
           <div className='catalog_categories'>
@@ -60,7 +63,8 @@ export default async function Page() {
             
           <div className='catalog_view_all'>
             <button className=''>
-                Смотреть все категории
+                <span dangerouslySetInnerHTML={categories_button()} />
+
                 <svg width="15" height="11" viewBox="0 0 15 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9.30864 9.625L14 5.625M14 5.625L9.30864 1.625M14 5.625L0.518518 5.625" stroke="#F97316" stroke-linecap="square"/>
                 </svg>
---
+++ services/catalog/page.js
@@ -0,0 +1,15 @@
+// ---------------------------------------- // 
+// ----- Данные для страницы каталога ----- //
+// ---------------------------------------- //
+export const getCatalogPageContent = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/catalog-page?populate=*`, { 
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+  
+    if (!response.ok) throw new Error("Unable to fetch posts.");
+  
+    return response.json();
+};
