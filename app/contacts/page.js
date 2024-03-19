f1ad6eef337a06ff4037ad82f8a7e41360c5a067
Никита <mnd1work@gmail.com>
2024-03-19 21:08:28 +0400

    catalog fix

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -89,7 +89,7 @@ export default function Page() {
                                 <button className="w-[30%] rounded-[5px] text-[20px] text-[#fff] bg-[#f97316]">Отправить</button>
                         </div>
 
-                        <div className="flex text-[#fff] justify-end"> <p className='w-[26%] text-center font-[500]'>Нажмите на кнопку Отправить, я соглашаюсь с <Link className='underline font-[300]'>офертой на обработку персональных данных.</Link></p> </div>
+                        <div className="flex text-[#fff] justify-end"> <p className='w-[26%] text-center font-[500]'>Нажмите на кнопку Отправить, я соглашаюсь с <Link href='#' className='underline font-[300]'>офертой на обработку персональных данных.</Link></p> </div>
 
                     </div>
                 </div> 
--- components/catalog/catalog.jsx
+++ components/catalog/catalog.jsx
@@ -93,4 +93,47 @@ export default function Catalog() {
           </section>
         )
     } 
+    else if(productsItems != null) {
+        return (
+            <section id='catalog' className='section_catalog relative pt-0'>            
+                <div className='catalog_main container m-auto'>
+                    {productsItems.data.map(product => {
+                        let count = 0;
+
+                        return (
+                            product.attributes.categories.data.map(product_category => {
+                                if((product_category.attributes.slug == category || category == 'all') && count <= 0) {
+                                    count++;
+                                    
+                                    return (
+                                        <div className='catalog_object' key={product.id}>
+                                            <div className='object_photo'>
+                                                <Image src={process.env.NEXT_PUBLIC_STRAPI_API_URL + product.attributes.image.data[0].attributes.url} alt='Изображение товара' width={364} height={320}/>
+                                            </div>
+            
+                                            <div className='object_desc'>
+                                                <h1>{product.attributes.name}</h1>
+                                                <p>В наличии - {product.attributes.count} шт.</p>
+                                                <p className='object_desc_text'>{product.attributes.description}</p>
+            
+                                                <div className='object_price'>
+                                                    <p>{product.attributes.price}</p>
+                                                    <p>₽</p>
+                                                </div>
+            
+                                                <div className='object_buttons'>
+                                                    <Link className='object_button1' href={'/catalog/' + product.attributes.slug}>Подробнее</Link>
+                                                    <button className='object_button2'>Добавить в корзину</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            })
+                        )
+                    })}
+                </div>
+            </section>
+        )
+    } 
 }
